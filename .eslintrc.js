46a26c6c226a2040e0d58b7b423b7d54d0d9f96d
Matheus Gritz <matheusgritz@gmail.com>
2020-07-12 15:25:12 -0300

    Update .eslint.js config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,9 @@
 module.exports = {
   'env': {
-    'browser': true,
+    'node': true,
     'commonjs': true,
-    'es2020': true
+    'es2020': true,
+    'jest': true
   },
   'extends': 'eslint:recommended',
   'parserOptions': {
@@ -24,6 +25,14 @@ module.exports = {
     'semi': [
       'error',
       'never'
+    ],
+    'eqeqeq': 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': [
+      'error', 'always'
+    ],
+    'arrow-spacing': [
+      'error', { 'before': true, 'after': true }
     ]
-  }
+  },
 }
