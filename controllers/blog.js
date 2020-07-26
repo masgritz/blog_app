3d0cccace82c1a6b7ee538f636633ed8f520ad1b
Matheus Gritz <matheusgritz@gmail.com>
2020-07-26 01:51:53 -0300

    Add GET by id, PUT and DELETE routes

--- controllers/blog.js
+++ controllers/blog.js
@@ -6,6 +6,15 @@ blogRouter.get('/', async (req, res) => {
   res.send(blogs)
 })
 
+blogRouter.get('/:id', async (req, res) => {
+  const blog = await Blog.findById(req.params.id)
+  if (blog) {
+    res.send(blog)
+  } else {
+    res.status(404).end()
+  }
+})
+
 blogRouter.post('/', async (req, res) => {
   const { title, author, url } = req.body
 
@@ -18,6 +27,25 @@ blogRouter.post('/', async (req, res) => {
 
   const savedBlog = await blog.save()
   res.send(savedBlog)
+})
+
+blogRouter.put('/:id', async (req, res) => {
+  const { title, author, url, likes } = req.body
+
+  const blog = {
+    title,
+    author,
+    url,
+    likes
+  }
+
+  const updatedBlog = Blog.findByIdAndUpdate(req.params.id, blog, { new: true })
+  res.send(updatedBlog)
+})
+
+blogRouter.delete('/:id', async (req, res) => {
+  await Blog.findByIdAndRemove(req.params.id)
+  res.status(204).end()
 })
 
 module.exports = blogRouter
