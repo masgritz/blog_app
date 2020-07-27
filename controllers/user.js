045112d1d6dfb7b1b167afdd2ea859c38c6f46da
Matheus Gritz <matheusgritz@gmail.com>
2020-07-27 16:49:19 -0300

    Restrict minimum password length to 8

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcrypt')
 const userRouter = require('express').Router()
 const User = require('../models/user')
+const logger = require('../utils/logger')
 
 userRouter.get('/', async (req, res) => {
   const users = await User.find({})
@@ -9,6 +10,11 @@ userRouter.get('/', async (req, res) => {
 
 userRouter.post('/', async (req, res) => {
   const { username, name, password } = req.body
+
+  if (password.length < 8) {
+    return logger.error('Invalid password! Must be at least 8 characters long.')
+  }
+
   const passwordHash = await bcrypt.hash(password, 10)
 
   const user = new User({
