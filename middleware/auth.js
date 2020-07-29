70db3af59887ba08ac7acccf518c7849c72aca79
Matheus Gritz <matheusgritz@gmail.com>
2020-07-28 23:47:04 -0300

    Better error handling for auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,16 +2,21 @@ const jwt = require('jsonwebtoken')
 const User = require('../models/user')
 
 const auth = async (req, res, next) => {
-  const token = req.header('Authorization').replace('Bearer ', '')
-  const decoded = jwt.verify(token, process.env.JWT_SECRET)
-  const user = await User.findById(decoded.id)
+  try {
+    const token = req.header('Authorization').replace('Bearer ', '')
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+    const user = await User.findById(decoded.id)
 
-  if(!user) {
-    throw new Error()
+    if (!user) {
+      throw new Error()
+    }
+    req.token = token
+    req.user = user
+    next()
+  } catch (error) {
+    res.status(401).end()
+    next()
   }
-  req.token = token
-  req.user = user
-  next()
 }
 
 module.exports = auth
